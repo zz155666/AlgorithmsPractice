f2151b4c052a5931340d975f11baf2d995e4b0ab
teerus <z83@foxmail.com>
2018-08-01 22:46:37 +0800

    队列与栈增加迭代代码

--- src/QueueNode.java
+++ src/QueueNode.java
@@ -50,4 +50,23 @@ public class QueueNode<Item> implements Iterable<Item> {
     public Iterator<Item> iterator() {
         return null;
     }
+    private class ListIterator implements Iterator<Item>{
+        private Node current=first;
+        @Override
+        public boolean hasNext() {
+            return current!=null;
+        }
+
+        @Override
+        public Item next() {
+            Item item=current.item;
+            current=current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+
+        }
+    }
 }
