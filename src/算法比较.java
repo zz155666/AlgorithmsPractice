dce994d5bafdc0730c0583e5275e95f36ea7bd0a
teerus <z83@foxmail.com>
2018-08-05 23:34:01 +0800

    算法执行效率比较

---
+++ src/算法比较.java
@@ -0,0 +1,47 @@
+/**
+ * @ProjectName: AlgorithmsPractice
+ * @Package: PACKAGE_NAME
+ * @ClassName: 算法比较
+ * @Description: 比较两种算法的执行效率
+ * @Author: teerus
+ * @CreateDate: 2018/8/5 23:09
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class 算法比较 {
+
+    public static double time (String alg,Double[] a){
+        Stopwatch timer=new Stopwatch();
+        if(alg.equals("Insertion")){
+            Inertion.sort(a);
+        }
+        if(alg.equals("Selection")){
+            Selection.sort(a);
+        }
+        return timer.elapsedTime();
+    }
+
+
+    public static double timeRandomInput(String alg,int N,int T){
+        double total=0.0;
+        Double[] a=new Double[N];
+        for(int t=0;t<T;t++){
+            for (int i=0;i<N;i++){
+                a[i]=StdRandom.uniform();
+            }
+            total+=time(alg,a);
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int N=10000;
+        int T=100;
+        double t1=timeRandomInput("Insertion",N,T);
+        double t2=timeRandomInput("Selection",N,T);
+
+        StdOut.println("第一个算法的时间为："+t1+"  第二个算法的时间为："+t2+"  他们的比值为："+t2/t1);
+
+    }
+
+}
