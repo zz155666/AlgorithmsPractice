0505ea0f053df1384fdc1fe664111667c66165f1
teerus <z83@foxmail.com>
2018-08-07 22:42:56 +0800

    自低向下的归并排序

--- src/MergeBU.java
+++ src/MergeBU.java
@@ -1,28 +1,24 @@
 /**
  * @ProjectName: AlgorithmsPractice
  * @Package: PACKAGE_NAME
- * @ClassName: 自顶向下的归并排序
+ * @ClassName: 自低向下的归并排序
  * @Description: java类作用描述
  * @Author: teerus
- * @CreateDate: 2018/8/7 21:08
+ * @CreateDate: 2018/8/7 22:29
  * @UpdateRemark: 更新说明
  * @Version: 1.0
  */
-public class Mergetion {
+public class MergeBU {
 
     private static Comparable[] aux;
     public static void sort(Comparable[] a){
+        int N=a.length;
         aux=new Comparable[a.length];
-        sort(a,0,a.length-1);
-    }
-    private static void sort(Comparable[] a,int lo,int hi){
-        if(hi<=lo){
-            return;
+        for(int sz=1;sz<N;sz=sz+sz){
+            for(int lo=0;lo<N-sz;lo+=sz+sz){
+                merge(a,lo,lo+sz+1,Math.min(lo+sz+sz-1,N-1));
+            }
         }
-        int mid=lo+(hi-lo)/2;
-        sort(a,lo,mid);
-        sort(a,mid+1,hi);
-        merge(a,lo,mid,hi);
     }
     private static  void merge(Comparable[] a,int lo,int mid,int hi){
         int i=lo,j=mid+1;
@@ -69,4 +65,5 @@ public class Mergetion {
         assert isShorted(a);
         show(a);
     }
+
 }
